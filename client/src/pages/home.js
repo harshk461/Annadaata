7cef48696f81d69a3637e55164c3b01d269274f3
Harsh <hk2152573@gmail.com>
2023-03-07 22:51:28 +0530

    commit

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import Drone from '../components/Drone/drone'
-import Footer from '../components/Footer/footer'
 import HomeHeader from '../components/HomeHeader/homeheader'
-import Navbar from '../components/Navbar/navbar'
 import Navbar2 from '../components/Navbar2/navbar2'
 
 export default function Home() {
